22714fcee3646ef4ea90fb58bc1555c143ed03c4
sunyongfei <251528801@qq.com>
2020-05-08 14:05:42 +0800

    修复-调整加载位置

--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,19 @@ import Storage from 'sync-storage-listener';
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import store from '@/store';
-import {getRightPath, createRoutesByStoreRoutes, createRoutesByUserInfo,splitRoutesByIsLogin} from '@/utils/routerMethods';
-import systemRoutes from './systemRoutes'
-import baseRoutes from './baseRoutes';
+import { getRightPath, createRoutesByStoreRoutes, createRoutesByUserInfo, splitRoutesByIsLogin } from '@/utils/routerMethods';
+import { baseRoutes, addRoutes } from '@/router/routes/index';
 import { deepCopy } from '@/utils/common'
 import { routerConfig } from '@/config/setting'
 import EventBus from '@/utils/eventbus';
-const copySystemRouter = deepCopy(systemRoutes)
-const [defaultRoutes,addSystemRoutes]=splitRoutesByIsLogin(baseRoutes,copySystemRouter,routerConfig,'/')
+const copyAddRoutes = deepCopy(addRoutes)
+const systemName = ''
+const [defaultRoutes, addSystemRoutes] = splitRoutesByIsLogin(baseRoutes, copyAddRoutes, routerConfig, `${systemName}/`)
 NProgress.configure({ showSpinner: false })
 Vue.use(VueRouter)
 EventBus.on('login', emitLogin)
 EventBus.on('logout', emitLogout)
-function emitLogin() {  
+function emitLogin() {
   store.dispatch('login', null)
 }
 function emitLogout() {
@@ -27,24 +27,24 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes: defaultRoutes,
 })
-router.beforeEach((to, from, next) => { 
+router.beforeEach((to, from, next) => {
   NProgress.start();
   Storage.ready().then(() => {
     const UserStore = {
       userToken: store.getters.userToken,
       userInfo: store.getters.userInfo
     }
-    let path = getRightPath(to, UserStore,routerConfig,'/error/404')
-    path ? next({path}) : next()
+    let path = getRightPath(to, UserStore, routerConfig, `${systemName}/error/404`)
+    path ? next({ path }) : next()
   })
 })
 router.afterEach(() => {
   NProgress.done()
 })
-export function fixRoutes(addRoutes, userRoutes) {  
-  return router.addRoutes(addRoutes || createRoutesByStoreRoutes(userRoutes,addSystemRoutes))
+export function fixRoutes(addRoutes, userRoutes) {
+  return router.addRoutes(addRoutes || createRoutesByStoreRoutes(userRoutes, addSystemRoutes))
 }
 export function makeRoutesByStoreInfo(userInfo) {
-  return createRoutesByUserInfo(userInfo,routerConfig,addSystemRoutes)
+  return createRoutesByUserInfo(userInfo, routerConfig, addSystemRoutes)
 }
 export default router
---
+++ src/router/routes/index.js
@@ -0,0 +1,3 @@
+import addRoutes from './addRoutes.js';
+import baseRoutes from './baseRoutes.js';
+export  { addRoutes, baseRoutes }
