7946c8e57ca33a892dcb684dcd2ae7f5cb06124f
sunyongfei <251528801@qq.com>
2020-05-09 15:27:15 +0800

    优化-增加打包分析

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import mountVue from '@/utils/mountVue'
 import { openMock, storageConfig, ajaxConfig } from '@/config/setting'
 Vue.config.productionTip = false
 Vue.use(mountVue, { storageConfig, ajaxConfig })
-openMock && require('@/mock')
+process.env.NODE_ENV !== 'production' && openMock && require('@/mock')
 new Vue({
   router,
   store,
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 const SystemConfig = require('./src/config/system.config')
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin; // 打包分析
+const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
 module.exports = {
     assetsDir: SystemConfig.assetsDir,
     publicPath: SystemConfig.publicPath,
-   
+    productionSourceMap: false,
     chainWebpack: config => {
         config
             .plugin('html')
@@ -10,6 +12,27 @@ module.exports = {
                 args[0].title = SystemConfig.title
                 return args
             })
-    }
+        if (IS_PROD) {
+            config.plugin("webpack-report").use(BundleAnalyzerPlugin, [
+                {
+                    analyzerMode: "static"
+                }
+            ]);
+            // 需要翻墙安装 npm i -D image-webpack-loader
+            // config.module
+            // .rule("images")
+            // .use("image-webpack-loader")
+            // .loader("image-webpack-loader")
+            // .options({
+            //     mozjpeg: { progressive: true, quality: 75 },
+            //     optipng: { enabled: false },
+            //     pngquant: { quality: [0.75, 0.9], speed: 4 },
+            //     gifsicle: { interlaced: false },
+            //     webp: { quality: 75 }
+            // });
+        }
+       
+    },
+
 }
 
